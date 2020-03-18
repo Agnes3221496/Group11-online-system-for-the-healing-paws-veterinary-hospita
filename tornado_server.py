f80781b175bb89921129765fd750dae03a1d42db
LtStarZq <42806293+LtStarZq@users.noreply.github.com>
2020-03-18 18:16:56 +0800

    Delete tornado_server.py

--- tornado_server.py
+++
@@ -1,8 +0,0 @@
-from tornado.httpserver import HTTPServer
-from tornado.wsgi import WSGIContainer
-from blogapp import app
-from tornado.ioloop import IOLoop
-
-s = HTTPServer(WSGIContainer(app))
-s.listen(5000)
-IOLoop.current().start()
