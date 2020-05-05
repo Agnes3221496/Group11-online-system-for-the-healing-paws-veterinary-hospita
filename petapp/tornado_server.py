46c4a1b3b852bf13a84e4b45e2d8727e44e74211
17205940 <951110228@qq.com>
2020-05-06 02:04:44 +0800

    update internationalization

--- petapp/tornado_server.py
+++ petapp/tornado_server.py
@@ -1,4 +1,22 @@
-<<<<<<< HEAD
+
+from tornado.httpserver import HTTPServer
+from tornado.wsgi import WSGIContainer
+from petapp import app
+from tornado.ioloop import IOLoop
+
+s = HTTPServer(WSGIContainer(app))
+s.listen(5000)
+IOLoop.current().start()
+
+from tornado.httpserver import HTTPServer
+from tornado.wsgi import WSGIContainer
+from blogapp import app
+from tornado.ioloop import IOLoop
+
+s = HTTPServer(WSGIContainer(app))
+s.listen(5000)
+IOLoop.current().start()
+
 
 from tornado.httpserver import HTTPServer
 from tornado.wsgi import WSGIContainer
@@ -18,24 +36,3 @@ s = HTTPServer(WSGIContainer(app))
 s.listen(5000)
 IOLoop.current().start()
 
-=======
-
-from tornado.httpserver import HTTPServer
-from tornado.wsgi import WSGIContainer
-from blogapp import app
-from tornado.ioloop import IOLoop
-
-s = HTTPServer(WSGIContainer(app))
-s.listen(5000)
-IOLoop.current().start()
-
-from tornado.httpserver import HTTPServer
-from tornado.wsgi import WSGIContainer
-from blogapp import app
-from tornado.ioloop import IOLoop
-
-s = HTTPServer(WSGIContainer(app))
-s.listen(5000)
-IOLoop.current().start()
-
->>>>>>> c1122cfb55214f1604deee58947c5a4bd3038adf
